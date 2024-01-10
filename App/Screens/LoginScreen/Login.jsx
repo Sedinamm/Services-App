656b0f9871d998f48187d23f057d593dd3b45eec
Sedinam <sedinam436@gmail.com>
2024-01-10 00:39:32 -0300

    imported Touchable Opacity

--- App/Screens/LoginScreen/Login.jsx
+++ App/Screens/LoginScreen/Login.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import Colors from "../../Utils/Colors";
 
