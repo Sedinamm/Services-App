5a8453385eac5f0e15e7eb3b13c09db9ff6605bd
Sedinam <sedinam436@gmail.com>
2024-01-15 22:49:53 -0300

    imported creatBottomTabNavigator

--- App/Navigations/TabNavigation.jsx
+++ App/Navigations/TabNavigation.jsx
@@ -1,5 +1,8 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
   return (
