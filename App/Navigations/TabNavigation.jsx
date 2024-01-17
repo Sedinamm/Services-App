9843ae1b3f90d10e68b1d172b2c517d871a092af
Sedinam <sedinam436@gmail.com>
2024-01-16 23:07:23 -0300

    Set the headerShown to 'false'

--- App/Navigations/TabNavigation.jsx
+++ App/Navigations/TabNavigation.jsx
@@ -9,7 +9,11 @@ const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Tab.Screen name="home" component={HomeScreen} />
       <Tab.Screen name="booking" component={BookingScreen} />
       <Tab.Screen name="profile" component={ProfileScreen} />
