9f5176f8cdb5ca91d42acd04f9414544aad74e0d
Sedinam <sedinam436@gmail.com>
2024-01-18 00:27:47 -0300

    added a User Image/Profile Picture

--- App/Screens/HomeScreen/Header.jsx
+++ App/Screens/HomeScreen/Header.jsx
@@ -1,10 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
+import { useUser } from "@clerk/clerk-expo";
 
 export default function Header() {
+  const { user, isLoading } = useUser();
   return (
-    <View>
-      <Text>Header</Text>
-    </View>
+    user && (
+      <View>
+        <View>
+          <Image source={{ uri: user?.imageUrl }} style={styles.userImage} />
+          <View></View>
+        </View>
+      </View>
+    )
   );
 }
+
+const styles = StyleSheet.create({
+  userImage: {
+    width: 45,
+    height: 45,
+  },
+});
