2e4e147f4ec78091b8e3ed27c51bcf7e402034ae
Sedinam <sedinam436@gmail.com>
2024-01-19 00:38:43 -0300

    imported FontAwesome

--- App/Screens/HomeScreen/Header.jsx
+++ App/Screens/HomeScreen/Header.jsx
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
 import { useUser } from "@clerk/clerk-expo";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function Header() {
   const { user, isLoading } = useUser();
