61192b1b08a6b0ee897d4ce6aa577d8f60c521e3
Sedinam <sedinam436@gmail.com>
2024-01-21 18:34:27 -0300

    installed graphql, graphql-request and react-is

---
+++ App/Utils/GlobalApi.js
@@ -0,0 +1,10 @@
+// import { request, gql } from 'graphql-request'
+
+// const document = gql`
+//   {
+//     company {
+//       ceo
+//     }
+//   }
+// `
+// await request('https://api.spacex.land/graphql/', document)
