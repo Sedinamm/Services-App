bb074dc3e020d23dcff1ed59f38b54e122a43674
Sedinam <sedinam436@gmail.com>
2024-01-22 12:47:41 -0300

    installed expo auth session expo random

--- App.js
+++ App.js
@@ -3,9 +3,29 @@ import { StyleSheet, Text, View } from "react-native";
 import Login from "./App/Screens/LoginScreen/Login";
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 
+// const tokenCache = {
+//   async getToken(key) {
+//     try {
+//       return SecureStore.getItemAsync(key);
+//     } catch (err) {
+//       return null;
+//     }
+//   },
+//   async saveToken(key, value) {
+//     try {
+//       return SecureStore.setItemAsync(key, value);
+//     } catch (err) {
+//       return;
+//     }
+//   },
+// };
+
 export default function App() {
   return (
-    <ClerkProvider publishableKey="pk_test_aGVhbHRoeS1tYXJtb3QtOS5jbGVyay5hY2NvdW50cy5kZXYk">
+    <ClerkProvider
+      // tokenCache={tokenCache}
+      publishableKey="pk_test_aGVhbHRoeS1tYXJtb3QtOS5jbGVyay5hY2NvdW50cy5kZXYk"
+    >
       <View style={styles.container}>
         {/* Sign in Component */}
         <SignedIn>
