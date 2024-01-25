d2c62f4dd6f747b8c5f616c96d4fbbcb2a3821b3
Sedinam <sedinam436@gmail.com>
2024-01-24 22:13:47 -0300

    Created a components folder and created a file called Heading.jsx

--- App/Screens/HomeScreen/Categories.jsx
+++ App/Screens/HomeScreen/Categories.jsx
@@ -1,14 +1,18 @@
 import { View, Text } from "react-native";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import GlobalApi from "../../Utils/GlobalApi";
 
 export default function Categories() {
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
     getCategories();
   }, []);
+
+  //   Get Categories List
   const getCategories = () => {
     GlobalApi.getCategories().then((resp) => {
-      console.log(resp);
+      //   console.log(resp)
+      setCategories(resp?.categories);
     });
   };
   return (
