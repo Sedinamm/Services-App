1ac91097c48209204b0b3730badf0423e845c2b9
Sedinam <sedinam436@gmail.com>
2024-01-24 22:08:47 -0300

    initial commit

--- App/Screens/HomeScreen/Categories.jsx
+++ App/Screens/HomeScreen/Categories.jsx
@@ -1,7 +1,16 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import GlobalApi from "../../Utils/GlobalApi";
 
 export default function Categories() {
+  useEffect(() => {
+    getCategories();
+  }, []);
+  const getCategories = () => {
+    GlobalApi.getCategories().then((resp) => {
+      console.log(resp);
+    });
+  };
   return (
     <View>
       <Text>Categories</Text>
