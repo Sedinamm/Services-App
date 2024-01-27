3b84d0366b53786ca0d4b8ed44e658ca26995e62
Sedinam <sedinam436@gmail.com>
2024-01-26 23:49:14 -0300

    added text

--- App/Screens/HomeScreen/BusinessList.jsx
+++ App/Screens/HomeScreen/BusinessList.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import Heading from "../../Components/Heading";
 
 export default function BusinessList() {
   return (
-    <View>
-      <Text>BusinessList</Text>
+    <View style={{ marginTop: 10 }}>
+      <Heading text={"Latest Business"} isViewAll={true} />
     </View>
   );
 }
