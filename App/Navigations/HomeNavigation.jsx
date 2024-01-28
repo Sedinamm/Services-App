192fea8ba96336de1612dd380ae04ba20dc2c166
Sedinam <sedinam436@gmail.com>
2024-01-28 16:32:56 -0300

    added some stack screens

--- App/Navigations/HomeNavigation.jsx
+++ App/Navigations/HomeNavigation.jsx
@@ -1,10 +1,18 @@
 import { View, Text } from "react-native";
+import HomeScreen from "../Screens/HomeScreen/HomeScreen";
 import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import BusinessListByCategoryScreen from "../Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen";
+const Stack = createStackNavigator();
 
 export default function HomeNavigation() {
   return (
-    <View>
-      <Text>HomeNavigation</Text>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="home" component={HomeScreen} />
+      <Stack.Screen
+        name="business-list"
+        component={BusinessListByCategoryScreen}
+      />
+    </Stack.Navigator>
   );
 }
