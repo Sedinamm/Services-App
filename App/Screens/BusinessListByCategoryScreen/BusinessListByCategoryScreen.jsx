6f2042d02f0472a735cc4da36b8c9fc03277c18e
Sedinam <sedinam436@gmail.com>
2024-01-29 17:07:09 -0300

    added back navigation to the BusinessListByCategories Screen

--- App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
+++ App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
@@ -1,16 +1,26 @@
 import { View, Text } from "react-native";
 import React, { useEffect } from "react";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function BusinessListByCategoryScreen() {
   const param = useRoute().params;
+  const navigation = useNavigation();
   useEffect(() => {
     console.log("Category", param.category);
   }, []);
   return (
-    <View>
-      <Ionicons name="arrow-back-outline" size={24} color="black" />
+    <View style={{ padding: 20, paddingTop: 30 }}>
+      <TouchableOpacity
+        style={{ display: "flex", flexDirection: "row", gap: 10 }}
+        onPress={() => navigation.goBack()}
+      >
+        <Ionicons name="arrow-back-outline" size={30} color="black" />
+        <Text style={{ fontSize: 25, fontFamily: "outfit-medium" }}>
+          {param?.category}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
