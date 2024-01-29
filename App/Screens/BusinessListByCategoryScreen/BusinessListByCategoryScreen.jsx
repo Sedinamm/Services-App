d825845064493ace07f4a5fc83b81d5f7bb3b5f7
Sedinam <sedinam436@gmail.com>
2024-01-29 16:56:12 -0300

    initial commit

--- App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
+++ App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
@@ -1,7 +1,12 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import { useRoute } from "@react-navigation/native";
 
 export default function BusinessListByCategoryScreen() {
+  const param = useRoute().params;
+  useEffect(() => {
+    console.log("Category", param.category);
+  }, []);
   return (
     <View>
       <Text>BusinessListByCategoryScreen</Text>
--- App/Screens/HomeScreen/Categories.jsx
+++ App/Screens/HomeScreen/Categories.jsx
@@ -9,9 +9,12 @@ import {
 import React, { useEffect, useState } from "react";
 import GlobalApi from "../../Utils/GlobalApi";
 import Heading from "../../Components/Heading";
+// import { useNavigation } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Categories() {
   const [categories, setCategories] = useState([]);
+  const navigation = useNavigation();
   useEffect(() => {
     getCategories();
   }, []);
@@ -32,7 +35,14 @@ export default function Categories() {
         numColumns={4}
         renderItem={({ item, index }) =>
           index <= 3 && (
-            <TouchableOpacity style={styles.container}>
+            <TouchableOpacity
+              style={styles.container}
+              onPress={() =>
+                navigation.push("business-list", {
+                  category: item.name,
+                })
+              }
+            >
               <View style={styles.iconsContainer}>
                 <Image
                   source={{ uri: item?.icon?.url }}
