cabe5d282df07547dfd5583fe0d8c08a1e8ccf25
Sedinam <sedinam436@gmail.com>
2024-01-29 16:59:42 -0300

    imported icons

--- App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
+++ App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native";
 import React, { useEffect } from "react";
 import { useRoute } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function BusinessListByCategoryScreen() {
   const param = useRoute().params;
@@ -9,7 +10,7 @@ export default function BusinessListByCategoryScreen() {
   }, []);
   return (
     <View>
-      <Text>BusinessListByCategoryScreen</Text>
+      <Ionicons name="arrow-back-outline" size={24} color="black" />
     </View>
   );
 }
